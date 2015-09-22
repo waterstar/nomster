85c0874fe3eb73db2d5c9def81138d9e435d3c6c
suising <suising@waterstar.sg>
2015-09-22 16:03:51 +0000

    enable user login, logout

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+	before_action :authenticate_user!, :only => [:new, :create]
+
 	def index
 		@places = Place.all
 		@places_per_page = @places.page(params[:page]).per(5)
@@ -9,7 +11,7 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-    	Place.create(place_params)
+    	current_user.places.create(place_params)
     	redirect_to root_path
   	end
 
