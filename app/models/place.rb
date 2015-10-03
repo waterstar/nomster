16caecb24202d6364b0da53ed14355960b5999d9
suising <suising@waterstar.sg>
2015-10-03 22:20:30 +0000

    shorten anyerrors?

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,16 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
     geocoded_by :address
-    after_validation :anyerror?
+    after_validation { geocode if self.errors.empty? }
 
     validates :name,:address,:description, :presence => true, :length => { :minimum => 3 }
-
-private
-    def anyerror?
-      if self.errors.any?
-        return
-      else
-        geocode
-      end
-    end
 end
