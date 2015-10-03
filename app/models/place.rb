332febdc1c18af35afc69742e8edace9dc0f7cbe
suising <suising@waterstar.sg>
2015-10-03 18:59:10 +0000

    convert geocode only if no validation error found

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,16 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
     geocoded_by :address
-    after_validation :geocode
+    after_validation :anyerror?
 
     validates :name,:address,:description, :presence => true, :length => { :minimum => 3 }
+
+private
+    def anyerror?
+      if self.errors.any?
+        return
+      else
+        geocode
+      end
+    end
 end
