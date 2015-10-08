d795d87e06d1cbf6f4db2a169addc71c2fd59ece
suising <suising@waterstar.sg>
2015-10-08 13:02:58 +0000

    make email content dynamic & code (disabled) to send out email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,16 @@ Nomster::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "example.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_ADDRESS'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
 end
