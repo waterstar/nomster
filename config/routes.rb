4458c82144dfa5bc445f61d333edd591e3b29d65
suising <suising@waterstar.sg>
2015-10-09 08:53:52 +0000

    add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Nomster::Application.routes.draw do
     resources :comments, :only => :create
     resources :photos, :only => :create
   end
-
+  resources :users, :only => :show
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
